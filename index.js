a694c986c1a08e3b729b27882cc7313060f0837d
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2021-12-03 17:23:16 -0800

    commit tree 66cea04e064fa3ea502c6f98d87bf06943c6ae81

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+function superbowlWin(array){
+    let winYear = array.find(item => item.result === "W");
+    if (winYear ===  undefined){
+        return undefined;
+    }
+    return winYear.year;
+}
